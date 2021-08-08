92372dfaebfbeef39927812f419085878dcee809
Lucas Gabriel <lucasbsb100@gmail.com>
2021-08-08 01:32:13 -0300

    Criação da classe Node, adição das arestas e nós clicaveis
    Co-authored-by: Hugo Bezerra        <hugoricardo.so.be@gmail.com>

--- graph.py
+++ graph.py
@@ -14,27 +14,64 @@ graph = {
 WIDTH = 256
 HEIGTH = 196
 
+class Node:
+    def __init__(self, key, x, y, col):
+        self.posx = x
+        self.posy = y
+        self.key = key
+        self.color = col
+
+
+    def update(self):
+        mouse_pos = [pyxel.mouse_x, pyxel.mouse_y]
+        node_pos = [self.posx, self.posy]
+
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON) and (math.dist(mouse_pos, node_pos) < 6):
+            print(f"Clicou no nózinho = {self.key} 😎")
+            self.color = ((self.color-5)%2)+6
+        
+    def draw(self):
+        pyxel.circ(self.posx, self.posy, 5, self.color)
+        pyxel.text(self.posx, self.posy, self.key, 8)
+
+
 class App:
     def __init__(self):
-        pyxel.init(WIDTH, HEIGTH, caption="Hello Pyxel")
+        pyxel.init(WIDTH, HEIGTH, caption="My traversal Coach")
         pyxel.mouse(True)
+        
+        self.nodes = []
+        num_nodes = graph.__len__()
+        for i in range(0, num_nodes):
+            x = pyxel.width/2+math.sin(math.radians((360/num_nodes)*i))*80
+            y = pyxel.height/2+math.cos(math.radians((360/num_nodes)*i))*50
+
+            self.nodes.append(Node(str(i), x, y, 7))
+            
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
+        for node in self.nodes:
+            node.update()
+
+
+        
+
     def draw(self):
         
         pyxel.cls(0)
 
-        num_nodes = graph.__len__()
+        
+        for node in self.nodes:
+           
+            for neighbours in graph[node.key]:
+                pyxel.line(node.posx, node.posy, self.nodes[int(neighbours)].posx, self.nodes[int(neighbours)].posy, 10)
 
-        for i in range(0, num_nodes):
-            x = pyxel.width/2+math.sin(math.radians((360/num_nodes)*i))*80
-            y = pyxel.height/2+math.cos(math.radians((360/num_nodes)*i))*50
 
-            pyxel.circ(x, y, 5, 7)
-            pyxel.text(x, y, str(i), 8)
+        for node in self.nodes:
+            node.draw()
 
 App()
